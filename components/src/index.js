edb059f42056d40c7d7740910c275a348ec8f3f3
Lee Wannacott <wannacottL@gmail.com>
2021-05-11 14:42:45 +1200

    Add approve reject to comments props.children

--- components/src/index.js
+++ components/src/index.js
@@ -2,28 +2,39 @@ import React from "react";
 import ReactDOM from "react-dom";
 import CommentDetail from "./CommentDetail";
 import faker from "faker";
+import ApprovalCard from "./ApprovalCard";
+
+if (module.hot) {
+  module.hot.accept();
+}
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail
-        author="Sam"
-        timeAgo="Today at 4:45PM"
-        textComment="nice blog"
-        avatar={faker.image.avatar()}
-      />
-      <CommentDetail
-        author="Jane"
-        timeAgo="Today at 2:00PM"
-        textComment="horrible blog"
-        avatar={faker.image.avatar()}
-      />
-      <CommentDetail
-        author="Alex"
-        timeAgo="Yesterday at 5:00PM"
-        textComment="okay blog"
-        avatar={faker.image.avatar()}
-      />
+      <ApprovalCard>
+        <CommentDetail
+          author="Sam"
+          timeAgo="Today at 4:45PM"
+          textComment="nice blog"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Jane"
+          timeAgo="Today at 2:00PM"
+          textComment="horrible blog"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Alex"
+          timeAgo="Yesterday at 5:00PM"
+          textComment="okay blog"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
     </div>
   );
 };
